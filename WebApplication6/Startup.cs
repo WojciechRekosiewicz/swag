96c921ba1e57aa4de33f43851d6bd20dae1d96fd
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-11-17 22:35:11 +0100

    uncomment swagger options in startup

--- WebApplication6/Startup.cs
+++ WebApplication6/Startup.cs
@@ -37,34 +37,32 @@ namespace WebApplication6
             services.AddControllers();
 
 
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo
-            //    {
-            //        Version = "v1",
-            //        Title = "ToDo API",
-            //        Description = "A simple example ASP.NET Core Web API",
-            //        TermsOfService = new Uri("https://example.com/terms"),
-            //        Contact = new OpenApiContact
-            //        {
-            //            Name = "Shayne Boyer",
-            //            Email = string.Empty,
-            //            Url = new Uri("https://twitter.com/spboyer"),
-            //        },
-            //        License = new OpenApiLicense
-            //        {
-            //            Name = "Use under LICX",
-            //            Url = new Uri("https://example.com/license"),
-            //        }
-            //    });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                //c.IncludeXmlComments(xmlPath);
 
-
-            //    // Set the comments path for the Swagger JSON and UI.
-            //    //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            //    //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-            //    //c.IncludeXmlComments(xmlPath);
-
-            //});
+            });
 
         }
 
@@ -80,15 +78,15 @@ namespace WebApplication6
             app.UseHttpsRedirection();
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
-            // app.UseSwagger();
+             app.UseSwagger();
 
-            //// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            //// specifying the Swagger JSON endpoint.
-            //app.UseSwaggerUI(c =>
-            //{
-            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            //  //  c.RoutePrefix = string.Empty;
-            //});
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+              //  c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
